ef888d5a63fcf5c72a94471f44045d40b131d355
iverson76 -Mac- <iverson.76years@gmail.com>
2013-09-06 13:36:34 +0900

    first commit

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -6,6 +6,46 @@ App::uses('AppController', 'Controller');
  * @property User $User
  */
 class UsersController extends AppController {
+	    // Authコンポーネント
+    var $components = array( 'Auth');
 
-
+    public function beforeFilter() {
+        parent::beforeFilter();
+        // 非ログイン時にも実行可能とする
+        $this->Auth->allow( array( 'signup', 'activate'));
+    }
+	
+	public function signup() {
+	    if (!empty( $this->data)){
+	        //  保存
+	        if( $this->User->save( $this->data)){
+	            // ユーザアクティベート(本登録)用URLの作成
+	            $url = FULL_BASE_URL                     // ドメイン
+	                . $this->request->webroot            // サブディレクトリ
+	                . $this->name . DS . 'activate' . DS // コントローラ/アクション
+	                . $this->User->id . DS               // ユーザID
+	                . $this->User->password .  DS ;// ハッシュ値
+	            //  メール送信
+	            //  return
+	            $this->Session->setFlash( '仮登録成功。メール送信しました。');
+	        } else {
+	            //  バリデーションエラーメッセージを渡す
+	            $this->Session->setFlash( '入力エラー');
+	        }
+	    }
+	}
+	
+	public function activate( $user_id = null, $in_hash = null) {
+	    // UserモデルにIDをセット
+	    $this->User->id = $user_id;
+	    if ($this->User->exists() && $in_hash == $this->User->getActivationHash()) {
+	    // 本登録に有効なURL
+	        // statusフィールドを0に更新
+	        $this->User->saveField( 'status', 0);
+	        $this->Session->setFlash( 'Your account has been activated.');
+	    }else{
+	    // 本登録に無効なURL
+	        $this->Session->setFlash( 'Invalid activation URL');
+	    }
+	}
 }
--- app/Model/User.php
+++ app/Model/User.php
@@ -4,76 +4,36 @@ App::uses('AppModel', 'Model');
  * User Model
  *
  */
+
+
 class User extends AppModel {
+    var $name = 'User';
+    var $useTable = 'users';
+    public $validate = array(
+        'username' => array(
+            // メールアドレスであること。
+            'validEmail' => array(
+                'rule' => array( 'email', true),
+                'message' => 'メールアドレスを入力して下さい'
+            ),
+            // 一意性チェック
+            'emailExists' => array(
+                'rule' => 'isUnique',
+                'message' => 'メールアドレスは既に登録されています'
+            ),
+        ),
+        'plain' => array(
+             // パスワード・確認パスワードの一致
+             'match' => array(
+                 'rule' => array( 'confirmPassword', 'plain', 'plain_confirm'),
+                 'message' => 'Passwords do not match'
+             ),
+        )
+    );
 
-/**
- * Display field
- *
- * @var string
- */
-	public $displayField = 'id';
-
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array(
-		'id' => array(
-			'alphanumeric' => array(
-				'rule' => array('alphanumeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-			'maxlength' => array(
-				'rule' => array('maxlength'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'nickname' => array(
-			'maxlength' => array(
-				'rule' => array('maxlength'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'email' => array(
-			'email' => array(
-				'rule' => array('email'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'password' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+    public function confirmPassword( $field, $plain, $plain_confirm) {
+        if ($plain == $plain_confirm) {
+          return true;
+        }
+    }
 }
